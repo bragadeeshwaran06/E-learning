36ee854bf9119f54bf1e1254fd423fb06bdd5bdd
Bragadeeshwaran <bragadeeshwaran@testpress.in>
2024-07-16 14:08:26 +0530

    Add CourseListView

--- courses/views.py
+++ courses/views.py
@@ -13,6 +13,11 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
 
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin 
+
+from django.db.models import Count
+from .models import Subject
+
 class OwnerMixin(object):
     def get_queryset(self):
         qs = super().get_queryset()
@@ -134,3 +139,42 @@ class ContentDeleteView(View):
         content.item.delete()
         content.delete()
         return redirect('module_content_list', module.id)
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                    id=module_id,
+                                    course__owner=request.user)
+        return self.render_to_response({'module': module})
+
+class ModuleOrderView(CsrfExemptMixin,JsonRequestResponseMixin,View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                    course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+class ContentOrderView(CsrfExemptMixin,JsonRequestResponseMixin,View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                    module__course__owner=request.user) \
+                                    .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+                    total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+                    total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
