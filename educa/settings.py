3b9d60f8dec340eeee3f24e18447e2abf9fbc5ea
Bragadeeshwaran <bragadeeshwaran@testpress.in>
2024-07-18 11:03:01 +0530

    Add API

--- educa/settings.py
+++ educa/settings.py
@@ -131,8 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CACHES = {
  'default': {
- 'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
- 'LOCATION': '127.0.0.1:11211',
+    'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+    'LOCATION': '127.0.0.1:11211',
  }
 }
 
